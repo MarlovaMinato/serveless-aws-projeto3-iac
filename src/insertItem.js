1ca1bb637c85507e8e0503bc7287bcdcfbf77c9f
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2023-10-14 03:12:00 -0300

    Serverless projeto IAC

---
+++ src/insertItem.js
@@ -0,0 +1,35 @@
+"usar estrito" ;
+
+const  { v4 }  =  requer ( "uuid" ) ;
+const  AWS  =  requer ( "aws-sdk" )
+
+const insertItem = async (event) => {
+    //module.exports.insertItem = async (event) => {
+    
+      const {item} = JSON.parse(event.body);
+      const createdAt = new Date().toISOString();
+      const id = v4()
+    
+      const dynamodb = new AWS.DynamoDB.DocumentClient();
+    
+      const newItem = {
+        id,
+        item,
+        createdAt,
+        itemStatus: false
+      }
+    
+      await dynamodb.put({
+        TableName: "ItemTable",
+        Item: newItem
+      }).promise()
+    
+      return {
+        statusCode: 200,
+        body: JSON.stringify(newItem),
+      };
+    };
+
+    module.exports = {
+        handler:insertItem
+    };
